99da8042ceb0f20e988ab844a1143f5cc777ef9e
Erastus Ruiru <erastusruiru11@gmail.com>
2021-06-29 23:13:21 +0300

    edit delete find and list todo items

--- api/todo.js
+++ api/todo.js
@@ -1,17 +1,67 @@
-import Todo from "../models/todo" ;
+import Todo from "../models/todo";
 
+export async function createTodo(todoData) {
+  const newTodo = new Todo(todoData);
+  if (newTodo) {
+    await newTodo.save();
+    return newTodo;
+  } else {
+    throw {
+      status: 400,
+      message: "Error during event",
+    };
+  }
+}
 
-export async function createTodo(todoData){
-    const newTodo = new Todo(todoData);
-    if(newTodo){
-        await newTodo.save();
-        return newTodo;
-    } else {
-        throw {
-            status:400,
-            message:"Error during event"
-        }
-    }
+export async function getTodo() {
+  const todos = await Todo.find();
+  if (todos.length === 0) {
+    throw {
+      status: 200,
+      message: "No todo items available",
+    };
+  } else {
+    return todos;
+  }
+}
+
+export async function getAsingleTodo(id) {
+  const todo = await Todo.findById(id);
+  if (!todo) {
+    throw {
+      status: 200,
+      message: "Todo item does not exist ",
+    };
+  } else {
+    return todo;
+  }
+}
+
+export async function deleteTodoItem(id) {
+  const todo = await Todo.findById(id);
+  if (todo) {
+    todo.remove();
+    return todo;
+  } else {
+    throw {
+      status: 404,
+      message: "todo not found",
+    };
+  }
 }
 
 
+export async function updateTodoItem(id){
+    const todo = await Todo.findById(id);
+    if(todo){
+        todo.completed = !todo.completed
+        todo.save();
+        return todo
+    } else {
+        throw{
+            status:404,
+            message:"todo item not found"
+        }
+    }
+
+}
