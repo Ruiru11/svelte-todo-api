218a65ca67a22be7058a3c6cf05b3ceb7611520d
Erastus Ruiru <erastusruiru11@gmail.com>
2021-07-12 21:21:21 +0300

    add validators for login and regestration inputs

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    verified:{
+        type:Boolean,
+        default:false
+    },
+    createDate:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("User",userSchema);
